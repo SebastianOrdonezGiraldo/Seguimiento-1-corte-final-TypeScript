d74c37781361b3ee6c22c4698f535f27e80b76a1
Sebastian Ordoñez Giraldo <sebastian789go@gmail.com>
2023-10-26 10:53:18 -0500

    update

--- Ejercicio3.ts
+++ Ejercicio3.ts
@@ -1,41 +1,35 @@
-let n1: number =30000;
+let n1: number = 30000;
+let costoApartamento: number = 100000;
 
 if (n1 === null || isNaN(n1)) {
   console.log(n1);
-  let tipoInmueble: number;
+  let tipoInmueble: number = 2;
 
-  // Solicitar al usuario que ingrese el tipo de inmueble (1 para Usado, 2 para Nuevo)
+  let comisionInmobiliaria: number = 0;
+
   while (true) {
-    tipoInmueble = parseInt(prompt("Escoja el tipo de inmueble:\n1. Usado\n2. Nuevo"));
-
     if (tipoInmueble === 1 || tipoInmueble === 2) {
       break;
     } else {
-      alert("Opción inválida. Por favor, ingrese 1 o 2.");
+      console.log("Opción inválida. Por favor, ingrese 1 o 2.");
     }
   }
 
-  let comisionInmobiliaria: number;
   let valorPropietario: number;
   let valorInmobiliaria: number;
 
-  // Calcular comisión de la inmobiliaria y otros valores
   if (tipoInmueble === 1) {
-    comisionInmobiliaria = 0.1 * costoApartamento; // 10% de comisión para inmuebles usados
+    comisionInmobiliaria = 0.1 * costoApartamento;
   } else if (tipoInmueble === 2) {
-    comisionInmobiliaria = 0.2 * costoApartamento; // 20% de comisión para inmuebles nuevos
+    comisionInmobiliaria = 0.2 * costoApartamento;
   }
 
   valorPropietario = costoApartamento - comisionInmobiliaria;
   valorInmobiliaria = comisionInmobiliaria;
 
-  // Mostrar los resultados al usuario
-  alert(`Resultados:
+  console.log(`Resultados:
 Valor que recibe el propietario: $${valorPropietario.toFixed(2)}
 Valor recibido por la inmobiliaria: $${valorInmobiliaria.toFixed(2)}`);
 
-  // Mensaje de despedida
-  alert("¡Gracias por utilizar nuestro servicio!");
-
-  // Fin del programa
+  console.log("¡Gracias por utilizar nuestro servicio!");
 }
