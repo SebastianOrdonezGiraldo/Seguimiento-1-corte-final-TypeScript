338e586bbf2ab69a5301c27e6e0825024a76f02b
Sebastian Ordoñez Giraldo <sebastian789go@gmail.com>
2023-11-02 09:12:36 -0500

    .

--- clases1.ts
+++ clases1.ts
@@ -2,11 +2,11 @@ class Employee {
     id: string;
     name: string;
     salary: number;
-postition: string;
-contract: string;
-constructor(id: string, name: string, position: number, salary: number) {}
+    postition: string;
+    contract: string;
+    constructor(id: string, name: string, position: number, salary: number) {}
 
     calculatedSalary() {}
     getContract() {}
 }
-const empleyee1 = new Employee("1092","juang",1000,1000)
+const empleyee1 = new Employee("1092", "juang", 1000, 1000);
